68a8485ca21eb8fe714ecd50471eaf93a51352de
P1naveen <naveenprabodha31@gmail.com>
2026-09-09 12:14:54 +0530

    feat: create Spring Boot service skeletons

---
+++ backend/contribution-service/src/main/java/com/lankaassist/contribution_service/ContributionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lankaassist.contribution_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContributionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContributionServiceApplication.class, args);
+	}
+
+}
